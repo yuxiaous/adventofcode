4a98b8002f8d2d7d63e5c46e3a484c86197837f4
root <root@DELL-INSPIRON-5457.localdomain>
2019-12-28 22:36:57 +0800

    2019 day 2

--- 2019/day2.py
+++ 2019/day2.py
@@ -52,21 +52,21 @@ def main():
         program = f.read().strip()
         codes = [int(x) for x in program.split(',')]
 
-    # Part 1
-    computer1 = IntcodeComputer(codes)
-    computer1.set_noun_verb(12, 2)
-    computer1.run()
-    print("Part1:", computer1.memory[0])
+        # Part 1
+        computer1 = IntcodeComputer(codes)
+        computer1.set_noun_verb(12, 2)
+        computer1.run()
+        print("Part1:", computer1.memory[0])
 
-    # Part 2
-    output = 19690720
-    for noun in range(0, 100):
-        for verb in range(0, 100):
-            computer2 = IntcodeComputer(codes)
-            computer2.set_noun_verb(noun, verb)
-            computer2.run()
-            if computer2.memory[0] == output:
-                print("Part2:", 100 * noun + verb)
+        # Part 2
+        output = 19690720
+        for noun in range(0, 100):
+            for verb in range(0, 100):
+                computer2 = IntcodeComputer(codes)
+                computer2.set_noun_verb(noun, verb)
+                computer2.run()
+                if computer2.memory[0] == output:
+                    print("Part2:", 100 * noun + verb)
 
 if __name__ == "__main__":
     main()
