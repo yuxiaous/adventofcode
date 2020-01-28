5ad5bff1775b796958f4d9f06faf39f476eb97dc
YUXIAO <yuxiaous@gmail.com>
2020-01-29 01:05:41 +0800

    2019 day 1

--- 2019/day1.py
+++ 2019/day1.py
@@ -2,36 +2,41 @@
 
 import math
 
-def requirements1(mass):
-    fuel = math.floor(mass / 3) - 2
-    return fuel
+class Day1:
+    def __init__(self, inputs):
+        self.masses = [int(x) for x in inputs.split('\n')]
 
-def requirements2(mass):
-    total = 0
-    fuel = mass
-    while True:
-        fuel = requirements1(fuel)
-        if fuel <= 0:
-            break
-        total += fuel
-    return total
+    def requirements1(self, mass):
+        fuel = math.floor(mass / 3) - 2
+        return fuel
+
+    def requirements2(self, mass):
+        total = 0
+        fuel = mass
+        while True:
+            fuel = self.requirements1(fuel)
+            if fuel <= 0:
+                break
+            total += fuel
+        return total
+
+    def part1(self):
+        sum = 0
+        for mass in self.masses:
+            sum += self.requirements1(mass)
+        return sum
+
+    def part2(self):
+        sum = 0
+        for mass in self.masses:
+            sum += self.requirements2(mass)
+        return sum
 
 def main():
-    with open('day1.txt') as f:
-        inputs = f.read().strip()
-        masses = [int(x) for x in inputs.split('\n')]
-
-        # Part 1
-        sum = 0
-        for mass in masses:
-            sum += requirements1(mass)
-        print("Part1:", sum)
-
-        # Part 2
-        sum = 0
-        for mass in masses:
-            sum += requirements2(mass)
-        print("Part2:", sum)
+    inputs = open('day1.txt').read().strip()
+    day1 = Day1(inputs)
+    print(f'Part 1: {day1.part1()}')
+    print(f'Part 2: {day1.part2()}')
 
 if __name__ == "__main__":
     main()
