4b542d2fb7e998579b3ab0cd0e5d9c5fcad7cc88
YUXIAO <yuxiaous@gmail.com>
2020-01-29 01:15:34 +0800

    2019 day 2

--- 2019/day2.py
+++ 2019/day2.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 
-import computer
+from computer import Intcode
 
-def main():
-    with open('day2.txt') as f:
-        program = f.read().strip()
-        codes = [int(x) for x in program.split(',')]
+class Day2:
+    def __init__(self, program):
+        self.codes = [int(x) for x in program.split(',')]
 
-        # Part 1
-        computer1 = computer.Intcode(codes)
-        computer1.set_noun_verb(12, 2)
-        computer1.run()
-        print("Part1:", computer1.memory[0])
+    def set_noun_verb(self, computer, noun, verb):
+        computer.memory[1] = noun
+        computer.memory[2] = verb
 
-        # Part 2
-        output = 19690720
+    def part1(self):
+        computer = Intcode(self.codes)
+        self.set_noun_verb(computer, 12, 2)
+        computer.run()
+        return computer.memory[0]
+
+    def part2(self):
+        OUTPUT = 19690720
         for noun in range(0, 100):
             for verb in range(0, 100):
-                computer2 = computer.Intcode(codes)
-                computer2.set_noun_verb(noun, verb)
-                computer2.run()
-                if computer2.memory[0] == output:
-                    print("Part2:", 100 * noun + verb)
+                computer = Intcode(self.codes)
+                self.set_noun_verb(computer, noun, verb)
+                computer.run()
+                if computer.memory[0] == OUTPUT:
+                    return 100 * noun + verb
+
+def main():
+    program = open('day2.txt').read().strip()
+    day2 = Day2(program)
+    print(f'Part 1: {day2.part1()}')
+    print(f'Part 2: {day2.part2()}')
 
 if __name__ == "__main__":
     main()
