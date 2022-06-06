96937c19fb7e534ba964d613c4f57f9e4abeb257
yuxiao <yuxiaous@gmail.com>
2022-06-07 01:31:35 +0800

    day22

--- 2021/day22.py
+++ 2021/day22.py
@@ -3,6 +3,7 @@
 import re
 
 input = open('day22.txt').read().strip()
+input = input.split('\n')
 
 
 def parse_step(entry):
@@ -15,44 +16,102 @@ def parse_step(entry):
     y2 = int(match.group(5))
     z1 = int(match.group(6))
     z2 = int(match.group(7))
-    return turn, (x1, x2), (y1, y2), (z1, z2)
+    return turn, x1, x2, y1, y2, z1, z2
 
 
 def limit(val, lim):
-    val_l, val_h = val
-    lim_l, lim_h = lim
-    if val_l > lim_h:
-        return
-    if val_h < lim_l:
-        return
-    return (max(val_l, lim_l), min(val_h, lim_h))
+    v1, v2 = val
+    l1, l2 = lim
+    return v1 in range(l1, l2+1) and v2 in range(l1, l2+1)
 
 
-def part1(input: str):
+def part1(input):
     region = set()
-    for entry in input.split('\n'):
-        turn, range_x, range_y, range_z = parse_step(entry)
-        range_x = limit(range_x, (-50, 50))
-        range_y = limit(range_y, (-50, 50))
-        range_z = limit(range_z, (-50, 50))
+    for entry in input:
+        turn, x1, x2, y1, y2, z1, z2 = parse_step(entry)
 
-        if turn == 'on' and range_x and range_y and range_z:
-            for x in range(range_x[0], range_x[1]+1):
-                for y in range(range_y[0], range_y[1]+1):
-                    for z in range(range_z[0], range_z[1]+1):
+        # check range
+        in_range_x = limit((x1, x2), (-50, 50))
+        in_range_y = limit((y1, y2), (-50, 50))
+        in_range_z = limit((z1, z2), (-50, 50))
+        if not in_range_x or not in_range_y or not in_range_z:
+            continue
+        # turn on cubes
+        if turn == 'on':
+            for x in range(x1, x2+1):
+                for y in range(y1, y2+1):
+                    for z in range(z1, z2+1):
                         region.add((x, y, z))
-
-        if turn == 'off' and range_x and range_y and range_z:
-            for x in range(range_x[0], range_x[1]+1):
-                for y in range(range_y[0], range_y[1]+1):
-                    for z in range(range_z[0], range_z[1]+1):
+        # turn off cubes
+        elif turn == 'off':
+            for x in range(x1, x2+1):
+                for y in range(y1, y2+1):
+                    for z in range(z1, z2+1):
                         if (x, y, z) in region:
                             region.remove((x, y, z))
     return len(region)
 
 
+class Cuboid:
+    def __init__(self, x1, x2, y1, y2, z1, z2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.z1 = z1
+        self.z2 = z2
+
+    def intersect(self, other):
+        x1 = max(self.x1, other.x1)
+        x2 = min(self.x2, other.x2)
+        y1 = max(self.y1, other.y1)
+        y2 = min(self.y2, other.y2)
+        z1 = max(self.z1, other.z1)
+        z2 = min(self.z2, other.z2)
+        if x1 < x2 and y1 < y2 and z1 < z2:
+            return Cuboid(x1, x2, y1, y2, z1, z2)
+
+    def split(self, other):
+        inte = self.intersect(other)
+        if inte:
+            return [
+                Cuboid(self.x1, inte.x1, self.y1, self.y2, self.z1, self.z2),
+                Cuboid(inte.x2, self.x2, self.y1, self.y2, self.z1, self.z2),
+                Cuboid(inte.x1, inte.x2, self.y1, inte.y1, self.z1, self.z2),
+                Cuboid(inte.x1, inte.x2, inte.y2, self.y2, self.z1, self.z2),
+                Cuboid(inte.x1, inte.x2, inte.y1, inte.y2, self.z1, inte.z1),
+                Cuboid(inte.x1, inte.x2, inte.y1, inte.y2, inte.z2, self.z2),
+            ]
+        else:
+            return [
+                Cuboid(self.x1, self.x2, self.y1, self.y2, self.z1, self.z2),
+            ]
+
+    def volume(self):
+        len_x = self.x2-self.x1
+        len_y = self.y2-self.y1
+        len_z = self.z2-self.z1
+        return len_x * len_y * len_z
+
+
 def part2(input):
-    pass
+    region: list[Cuboid] = []
+    for entry in input:
+        turn, x1, x2, y1, y2, z1, z2 = parse_step(entry)
+
+        cuboid = Cuboid(x1, x2+1, y1, y2+1, z1, z2+1)
+
+        new_region = []
+        for on in region:
+            for chip in on.split(cuboid):
+                new_region.append(chip)
+
+        if turn == 'on':
+            new_region.append(cuboid)
+
+        region = new_region
+
+    return sum(x.volume() for x in region)
 
 
 if __name__ == '__main__':
