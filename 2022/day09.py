157122d70c2f40b69c3a0ed2737259cb54b85439
YUXIAO <yuxiaous@gmail.com>
2022-12-22 15:23:01 +0800

    day09

--- 2022/day09.py
+++ 2022/day09.py
@@ -5,37 +5,64 @@ input = open("day09.txt").read().strip()
 
 
 class Rope:
-    def __init__(self) -> None:
+    def __init__(self, length) -> None:
         self.start = (0, 0)
-        self.head = self.start
-        self.tail = self.start
+        self.knots = [list(self.start) for _ in range(length)]
 
     def move(self, direction):
         # head
+        head = self.knots[0]
         if direction == "U":
-            self.head = (self.head[0], self.head[1] + 1)
+            head[1] += 1
         elif direction == "D":
-            self.head = (self.head[0], self.head[1] - 1)
+            head[1] -= 1
         elif direction == "L":
-            self.head = (self.head[0] - 1, self.head[1])
+            head[0] -= 1
         elif direction == "R":
-            self.head = (self.head[0] + 1, self.head[1])
+            head[0] += 1
 
-        # tail
-        if self.head[0] - self.tail[0] > 1:
-            self.tail = (self.head[0] - 1, self.head[1])
-        elif self.head[0] - self.tail[0] < -1:
-            self.tail = (self.head[0] + 1, self.head[1])
-        elif self.head[1] - self.tail[1] > 1:
-            self.tail = (self.head[0], self.head[1] - 1)
-        elif self.head[1] - self.tail[1] < -1:
-            self.tail = (self.head[0], self.head[1] + 1)
+        # each knot further down
+        for i in range(1, len(self.knots)):
+            tail = self.knots[i]
+            head = self.knots[i - 1]
+
+            if max(abs(head[0] - tail[0]), abs(head[1] - tail[1])) > 1:
+                if head[1] > tail[1]:
+                    tail[1] += 1
+                elif head[1] < tail[1]:
+                    tail[1] -= 1
+                if head[0] > tail[0]:
+                    tail[0] += 1
+                elif head[0] < tail[0]:
+                    tail[0] -= 1
+
+        return tuple(self.knots[-1])
+
+    def visualize(self, range_x, range_y):
+        for y in reversed(range_y):
+            for x in range_x:
+                while True:
+                    position = (x, y)
+                    knots = [tuple(x) for x in self.knots]
+                    if position in knots:
+                        i = knots.index(position)
+                        print(i if i > 0 else "H", end="")
+                        break
+
+                    if position == self.start:
+                        print("s", end="")
+                        break
+
+                    print(".", end="")
+                    break
+
+            print("")
+        print("")
 
 
-def part1():
+def simulate(rope: Rope):
     visitd = set()
 
-    rope = Rope()
     for motion in input.split("\n"):
         match = re.match(r"([UDLR]) (\d+)", motion)
         if match:
@@ -43,14 +70,18 @@ def part1():
             step = int(match.groups()[1])
 
             for _ in range(step):
-                rope.move(direction)
-                visitd.add(rope.tail)
+                tail = rope.move(direction)
+                visitd.add(tail)
 
     return len(visitd)
 
 
+def part1():
+    return simulate(Rope(2))
+
+
 def part2():
-    pass
+    return simulate(Rope(10))
 
 
 if __name__ == "__main__":
